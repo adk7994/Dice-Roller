f613d305e2bd0186af7993db3d11165bbe51ec2c
adk7994 <146487012+adk7994@users.noreply.github.com>
2023-10-16 14:59:40 -0500

    Add files via upload

---
+++ diceProject.py
@@ -0,0 +1,21 @@
+import random
+#imports the random number library
+rolls = int(input("How many times do you want to roll?"))
+#Asks the user for number of dice rolls and converts that string to an integer
+diceType =int( input("How many sides do you want your dice to have?"))
+#asks the user for number of sides on the dice roll and converts the string to an integer
+for rollz in range (rolls):
+    #creates the loop within the amount of rolls set by the user
+    randomDice = random.randrange(diceType)+1
+    #ensures the rolls are to the specifications of the user
+    print ("Your roll was:")
+    print (randomDice)
+    #prints the outputs of the dice rolls
+    if diceType == 20:
+        #checks if the dice has 20 sides
+        if randomDice==20:
+            print("Critical hit!")
+            #checks if the 20 sided dice roll was a 20
+        elif randomDice==1:
+            print("Critical Failure")
+            #checks if the 20 sided dice roll was a 1
